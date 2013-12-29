f20699fc56e555ee752fcd97db994715cf3c4a03
eldarerathis <eldarerathis@gmail.com>
2013-12-29 16:29:53 -0500

    Make sure mSuspendBlocker.release() gets called

--- src/com/eldarerathis/xposedmodule/wirelesschargingxposed/WirelessChargingXposed.java
+++ src/com/eldarerathis/xposedmodule/wirelesschargingxposed/WirelessChargingXposed.java
@@ -2,6 +2,7 @@ package com.eldarerathis.xposedmodule.wirelesschargingxposed;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
+import static de.robv.android.xposed.XposedHelpers.callMethod;
 import android.content.Context;
 import android.os.Vibrator;
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -9,6 +10,9 @@ import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class WirelessChargingXposed implements IXposedHookLoadPackage
 {
@@ -29,7 +33,6 @@ public class WirelessChargingXposed implements IXposedHookLoadPackage
 						
 						LogD("Hooking playWirelessChargingStartedSound()");
 						
-						
 						if (mPrefs.getBoolean("pref_key_sound", false))
 						{
 							LogD("Charging started - playing notification sound");
@@ -47,11 +50,47 @@ public class WirelessChargingXposed implements IXposedHookLoadPackage
 							v.vibrate(pattern, -1);
 						}
 						
+						releaseSuspendBlocker(param.thisObject);
+						
 						return null;
 					}
 					
 				}
 		);
+	}
+	
+	private void releaseSuspendBlocker(Object thisObject)
+	{
+		Object suspendBlocker = getObjectField(thisObject, "mSuspendBlocker");
+		
+		if (suspendBlocker == null)
+		{
+			LogD("Couldn't find mSuspendBlocker; falling back to invoking finishPendingBroadcastLocked()");
+			
+			callMethod(thisObject, "finishPendingBroadcastLocked");
+		}
+		else
+		{
+			try 
+			{
+				Method m = suspendBlocker.getClass().getMethod("release");
+				m.invoke(thisObject);
+			} 
+			catch (NoSuchMethodException e) { handleInvokeException(e, thisObject); }
+			catch (IllegalArgumentException e) { handleInvokeException(e, thisObject); }
+			catch (IllegalAccessException e) { handleInvokeException(e, thisObject); }
+			catch (InvocationTargetException e) { handleInvokeException(e, thisObject); }
+		}
+	}
+	
+	private void handleInvokeException(Exception e, Object thisObject)
+	{
+		e.printStackTrace();
+		
+		LogD(e.getLocalizedMessage());
+		LogD("Error invoking mSuspendBlocker.release(); falling back to invoking finishPendingBroadcastLocked()");
+		
+		callMethod(thisObject, "finishPendingBroadcastLocked");
 	}
 
 	private void LogD(String message)
