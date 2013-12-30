2ea5e24c3a1ea53dcabe69a2048687740d8f0ef1
eldarerathis <eldarerathis@gmail.com>
2013-12-29 22:35:55 -0500

    Add settings for custom notification sounds

--- src/com/eldarerathis/xposedmodule/wirelesschargingxposed/Settings.java
+++ src/com/eldarerathis/xposedmodule/wirelesschargingxposed/Settings.java
@@ -1,19 +1,35 @@
 package com.eldarerathis.xposedmodule.wirelesschargingxposed;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
 
-public class Settings extends PreferenceFragment implements OnSharedPreferenceChangeListener
+public class Settings extends PreferenceFragment implements OnSharedPreferenceChangeListener, OnPreferenceChangeListener
 {
+	private Activity mActivity = null;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) 
 	{
         super.onCreate(savedInstanceState);
         getPreferenceManager().setSharedPreferencesMode(1);
         addPreferencesFromResource(R.xml.settings);
+        
+        RingtonePreference p = (RingtonePreference)findPreference("pref_key_custom_ringtone");
+        p.setOnPreferenceChangeListener(this);
+        
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mActivity);
+        updateRingtoneSummary(p, prefs.getString(p.getKey(), null));
     }
 	
 	@Override
@@ -22,14 +38,35 @@ public class Settings extends PreferenceFragment implements OnSharedPreferenceCh
         updateSummary(key);
     }
 	
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object newValue)
+	{
+		updateRingtoneSummary((RingtonePreference)preference, (String)newValue);
+		return true;
+	}
+	
 	private void updateSummary(String key) 
 	{
-        if(findPreference(key) instanceof CheckBoxPreference) {
+        if(findPreference(key) instanceof CheckBoxPreference) 
+        {
             CheckBoxPreference p = (CheckBoxPreference)findPreference(key);
             String summary = p.isChecked() ? p.getSummaryOn().toString() : p.getSummaryOff().toString();
             p.setSummary(summary);
         }
     }
+	
+	private void updateRingtoneSummary(RingtonePreference pref, String newValue)
+	{
+		if (mActivity != null)
+    	{
+        	Ringtone ringtone = RingtoneManager.getRingtone(mActivity, Uri.parse(newValue));
+        	
+        	if (ringtone != null)
+        		pref.setSummary(ringtone.getTitle(mActivity));
+        	else
+        		pref.setSummary("None");
+    	}
+	}
 	
 	@Override
     public void onResume() 
@@ -43,5 +80,12 @@ public class Settings extends PreferenceFragment implements OnSharedPreferenceCh
     {
         super.onPause();
         getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+    
+    @Override
+    public void onAttach(Activity activity)
+    {
+    	super.onAttach(activity);
+    	mActivity = activity;
     }
 }
--- src/com/eldarerathis/xposedmodule/wirelesschargingxposed/WirelessChargingXposed.java
+++ src/com/eldarerathis/xposedmodule/wirelesschargingxposed/WirelessChargingXposed.java
@@ -4,6 +4,10 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 import static de.robv.android.xposed.XposedHelpers.callMethod;
 import android.content.Context;
+import android.media.AudioManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Vibrator;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -30,21 +34,24 @@ public class WirelessChargingXposed implements IXposedHookLoadPackage
 					protected Object replaceHookedMethod(MethodHookParam param) throws Throwable 
 					{
 						mPrefs.reload();
+						Context ctxt = (Context)getObjectField(param.thisObject, "mContext");
 						
 						LogD("Hooking playWirelessChargingStartedSound()");
 						
 						if (mPrefs.getBoolean("pref_key_sound", false))
 						{
 							LogD("Charging started - playing notification sound");
-							
-							XposedBridge.invokeOriginalMethod(param.method, param.thisObject, null);
+						
+							if (mPrefs.getBoolean("pref_key_custom_sound", false))
+								playNotificationSound(ctxt);
+							else
+								XposedBridge.invokeOriginalMethod(param.method, param.thisObject, null);
 						}
 						
 						if (mPrefs.getBoolean("pref_key_vibrate", false))
 						{
 							LogD("Charging started - vibrating");
-							
-							Context ctxt = (Context)getObjectField(param.thisObject, "mContext");
+
 							Vibrator v = (Vibrator) ctxt.getSystemService(Context.VIBRATOR_SERVICE);
 							long[] pattern = new long[] { 0, 100, 100, 100 };
 							v.vibrate(pattern, -1);
@@ -57,6 +64,25 @@ public class WirelessChargingXposed implements IXposedHookLoadPackage
 					
 				}
 		);
+	}
+	
+	private void playNotificationSound(Context ctxt)
+	{
+		final String soundPath = mPrefs.getString("pref_key_custom_ringtone", null);
+		
+        if (soundPath != null && !soundPath.equals("")) 
+        {
+            final Uri soundUri = Uri.parse(soundPath);
+            if (soundUri != null) 
+            {
+                final Ringtone sfx = RingtoneManager.getRingtone(ctxt, soundUri);
+                if (sfx != null) 
+                {
+                    sfx.setStreamType(AudioManager.STREAM_NOTIFICATION);
+                    sfx.play();
+                }
+            }
+        }
 	}
 	
 	private void releaseSuspendBlocker(Object thisObject)
